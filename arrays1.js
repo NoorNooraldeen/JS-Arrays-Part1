3bc3f50a47984d1b35c1889ba5272c7b24c5854b
NoorNooraldeen <noornooraldeen6@outlook.com>
2024-10-08 10:14:13 +0300

    first

---
+++ arrays1.js
@@ -0,0 +1,16 @@
+const grocries = ["bread", "meat", "chiken", "eggs", "rice", "pasta"];
+console.log(grocries[1]);
+console.log(grocries.length);
+console.log(grocries[grocries.length - 1]);
+console.log(grocries.pop());
+grocries.push("ice", "bnana");
+const firstThreeItems = grocries.slice(0, 3);
+//console.log(firstThreeItems)
+
+//console.loggrocries.push("juice", "fruits");
+// console.log(grocries.length);
+// const firstThreeItems = grocries.slice(0, 3);
+
+// const thirdElemnt = grocries.pop(3);
+// grocries.push("ice");
+// console.log(grocries.length);
